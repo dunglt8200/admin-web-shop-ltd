47a85965d6d26edf938db31ca5e26928ef44e7a4
thanhdung <ltdung0802@gmail.com>
2025-01-07 17:34:16 +0700

    UI menu

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,9 +1,61 @@
-import React from "react";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ChaletIcon from '@mui/icons-material/Chalet';
+import AppsIcon from '@mui/icons-material/Apps';
+import CycloneIcon from '@mui/icons-material/Cyclone';
+
+const drawerWidth = 240;
+const listMenu = [
+    {
+        name: "Trang chủ",
+        icon: <ChaletIcon />
+    },
+    {
+        name: "Danh mục",
+        icon: <AppsIcon />
+    }
+]
 
 function Menu() {
-    return (
-        <>Menu</>
-    )
+  return (
+    <Box className="box-main-menu">
+      <CssBaseline />
+      <Drawer
+        variant="permanent"
+        className="drawer-menu"
+      >
+        <Box className="box-logo-menu">
+           <CycloneIcon className="ico-logo-menu"/>
+           <Typography className="text-logo-menu">Admin Site</Typography>
+        </Box>
+        <Divider />
+        <Box sx={{ overflow: 'auto' }}>
+          <List>
+            {listMenu.map((menu, index) => (
+                <ListItem key={index} disablePadding>
+                    <ListItemButton>
+                        <ListItemIcon className="ico-menu-item">
+                            {menu.icon}
+                        </ListItemIcon>
+                        <ListItemText primary={menu.name} />
+                    </ListItemButton>
+                </ListItem>
+            ))}
+          </List>
+          <Divider />          
+        </Box>
+      </Drawer>
+    </Box>
+  );
 }
 
 export default Menu
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/main/App'
 import {BrowserRouter} from 'react-router-dom'
+import "../src/assets/css/index.css"
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
