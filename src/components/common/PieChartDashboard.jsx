2b5975c5754f3bf23edfd8141b0a6b2a5f63dd9b
thanhdung <ltdung0802@gmail.com>
2025-01-22 12:00:54 +0700

    UI chart

---
+++ src/components/common/PieChartDashboard.jsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { PieChart } from '@mui/x-charts';
+
+export default function PieChartDashboard({data}) {
+  return (
+    <PieChart
+      series={[
+        {
+          data: data,
+          highlightScope: { fade: 'global', highlight: 'item' },
+          faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
+          valueFormatter
+        },
+      ]}
+      height={200}
+      slotProps={{
+        legend: {
+          labelStyle:{
+            fill: 'white'
+          }
+        },
+      }}
+    />
+  );
+}
+
+export const valueFormatter = (item) => `${item.value}%`;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,23 @@ import { Box } from "@mui/system";
 import React from "react";
 import BoxItemCountHome from "../../components/common/BoxItemCountHome";
 import BadgeIcon from '@mui/icons-material/Badge';
+import PieChartDashboard from "../../components/common/PieChartDashboard";
+import { Typography } from "@mui/material";
+
+const categorys = [
+    {
+      label: 'Nike',
+      value: 72.72,
+    },
+    {
+      label: 'Adidas',
+      value: 16.38,
+    },
+    {
+      label: 'Puma',
+      value: 3.83,
+    }
+  ];
 
 function Home() {
     return (
@@ -13,9 +30,18 @@ function Home() {
                 <BoxItemCountHome icon={<BadgeIcon sx={{ backgroundColor: "#AEB9E1" }}/>} title={"Tổng người truy cập"} value={"22"} isIncrease={false} percentValue={"4,2%"}/>
             </Box>
             <Box className="box-chart-home">
-                <Box>
-                    
+                <Box className="box-chart-product">
+
                 </Box>
+                <Box className="box-chart-group">
+                    <Box className="box-chart-category">
+                      <PieChartDashboard data={categorys}/>                   
+                      <Typography sx={{ color: "#fff" }}>Thống kê sản phẩm theo loại</Typography>
+                    </Box>
+                    <Box className="box-chart-bill">
+
+                    </Box>
+                </Box>               
             </Box>       
         </Box>
     )
